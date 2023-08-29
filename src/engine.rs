517cd83ad641c98fcfecc3e348356b5b399d4525
Ben McAvoy <Ben.McAvoy@tutanota.com>
2023-08-29 15:54:08 +0100

    Setup a basic engine structure

---
+++ src/engine.rs
@@ -0,0 +1,8 @@
+#[derive(Default)]
+pub struct Llanite {}
+
+impl Llanite {
+    pub fn start(&self) {
+        todo!("Start engine!")
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
-fn main() {
-    println!("Hello, world!");
-}
+mod engine;
+
+pub use engine::Llanite;
