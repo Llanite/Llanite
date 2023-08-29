93934aba139825fffc32bd7dacf9715db06a59bd
Ben McAvoy <Ben.McAvoy@tutanota.com>
2023-08-29 16:02:27 +0100

    Make the start function take in a config

--- src/engine.rs
+++ src/engine.rs
@@ -1,8 +1,12 @@
+mod config;
+
+use config::Config;
+
 #[derive(Default)]
 pub struct Llanite {}
 
 impl Llanite {
-    pub fn start(&self) {
+    pub fn start(&self, config: Config) {
         todo!("Start engine!")
     }
 }
