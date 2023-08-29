cd39502de678ce284403689fe0551f651f7f5097
Ben McAvoy <Ben.McAvoy@tutanota.com>
2023-08-29 16:34:37 +0100

    Try and kick off the booster when starting the engine

--- src/engine.rs
+++ src/engine.rs
@@ -1,7 +1,10 @@
 use crate::config::Config;
+use crate::booster::booster;
 
-use std::sync::{Arc, Mutex, MutexGuard};
+use std::sync::{Arc, Mutex};
 use ecs_rust::world::World;
+
+use tracing::error;
 
 pub struct Llanite {
     world: Arc<Mutex<World>>,
@@ -17,6 +20,8 @@ impl Default for Llanite {
 
 impl Llanite {
     pub fn start(&self, config: Config) {
-        todo!("Start engine!")
+        if let Err(e) = booster::initiate(config) {
+            error!("Error starting window: {e}");
+        }
     }
 }
