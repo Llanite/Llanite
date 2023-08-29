1343a858ec2404e43145d3e13b40e0578eda82a5
Ben McAvoy <Ben.McAvoy@tutanota.com>
2023-08-29 16:39:42 +0100

    create a new window

--- src/booster.rs
+++ src/booster.rs
@@ -3,6 +3,11 @@
 use crate::errors::BoosterError;
 use crate::config::Config;
 
+use winit::dpi::PhysicalSize;
+use winit::event::*;
+use winit::event_loop::{ControlFlow, EventLoop};
+use winit::window::WindowBuilder;
+
 /// Call the asynchronous launch function.
 pub fn initiate(config: Config) -> Result<(), BoosterError> {
     pollster::block_on(self::launch(config))?;
@@ -12,5 +17,14 @@ pub fn initiate(config: Config) -> Result<(), BoosterError> {
 
 /// Create the event loop and start up.
 pub async fn launch(config: Config) -> Result<(), BoosterError> {
+    let (width, height) = (config.width, config.height);
+
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new()
+        .with_inner_size(PhysicalSize::new(width, height))
+        .with_title(config.window_name)
+        .build(&event_loop)
+        .map_err(|_| BoosterError::WindowFailure)?;
+
     todo!("Launch!")
 }
