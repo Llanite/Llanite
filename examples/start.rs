75f1e74ed12321f95df6115089fc9d287585706b
Ben McAvoy <Ben.McAvoy@tutanota.com>
2023-08-31 16:51:31 +0100

    Change the config creation in the start example

--- examples/start.rs
+++ examples/start.rs
@@ -4,11 +4,9 @@ use llanite::Llanite;
 fn main() {
     let llanite = Llanite::default();
 
-    let config = Config {
+    llanite.start(Config {
         window_name: String::from("Start example"),
         height: 480,
         width: 640,
-    };
-
-    llanite.start(config);
+    });
 }
