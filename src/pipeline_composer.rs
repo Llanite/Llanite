79f11ea81b83cf56047cbcdcf734640522fe3a85
Ben McAvoy <Ben.McAvoy@tutanota.com>
2023-08-31 16:09:08 +0100

    Add a `new` function to `PipelineComposer`

--- src/pipeline_composer.rs
+++ src/pipeline_composer.rs
@@ -1,8 +1,22 @@
-use wgpu::{Device, RenderPipeline, SurfaceConfiguration};
+use wgpu::{Device, RenderPipeline, SurfaceConfiguration, FragmentState};
 use crate::errors::PipelineError;
 use std::{fs, sync::Arc};
 use tracing::info;
 
 pub struct PipelineComposer {
     pub(crate) pipeline: Result<RenderPipeline, PipelineError>,
+
+    config: SurfaceConfiguration,
+    device: Arc<Device>,
+}
+
+impl PipelineComposer {
+    pub fn new(device: Arc<Device>, config: SurfaceConfiguration) -> Self {
+        Self {
+            pipeline: Err(PipelineError::NotInitialised),
+
+            device,
+            config,
+        }
+    }
 }
