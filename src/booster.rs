29e2dc165a46016343d437ace95b2ef22f24397f
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-10 17:03:19 +0100

    Modify the booster into a struct with state as a field

--- src/booster.rs
+++ src/booster.rs
@@ -1,98 +1,106 @@
-use crate::config::Config;
 /// Booster. The file where everything kicks off in the engine loop.
-use crate::errors::BoosterError;
+use crate::config::Config;
 use crate::state::State;
 
-use winit::dpi::PhysicalSize;
-use winit::event::*;
 use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
+use winit::dpi::PhysicalSize;
+use winit::event::*;
 
-use tracing::{warn, error, Level};
 use tracing_subscriber::fmt::time::LocalTime;
 use time::macros::format_description;
 
-/// Call the asynchronous launch function.
-pub fn initiate(config: Config) -> Result<(), BoosterError> {
-    let timer = LocalTime::new(format_description!("[hour]:[minute]:[second]"));
+use tracing::{warn, error, Level};
 
-    tracing_subscriber::fmt()
-        .with_max_level(Level::INFO)
-        .with_thread_names(true)
-        .with_timer(timer)
-        .init();
+use std::sync::{Arc, Mutex};
 
-    pollster::block_on(self::launch(config))?;
+use anyhow::Result;
 
-    Ok(())
+pub struct Booster {
+    pub(crate) state: Option<Arc<Mutex<State<'static>>>>,
 }
 
-/// Create the event loop and start up.
-pub async fn launch(config: Config) -> Result<(), BoosterError> {
-    let (width, height) = (config.width, config.height);
+impl Booster {
+    pub fn new() -> Result<Booster> {
+        let timer = LocalTime::new(format_description!("[hour]:[minute]:[second]"));
 
-    let event_loop = EventLoop::new();
-    let window = WindowBuilder::new()
-        .with_inner_size(PhysicalSize::new(width, height))
-        .with_title(config.window_name)
-        .build(&event_loop)
-        .map_err(|_| BoosterError::WindowFailure)?;
+        tracing_subscriber::fmt()
+            .with_max_level(Level::INFO)
+            .with_thread_names(true)
+            .with_timer(timer)
+            .init();
 
-    let mut state = State::new(window).await?;
+        Ok(Self { state: None })
+    }
 
-    event_loop.run(move |event, _, control_flow| match event {
-        Event::WindowEvent {
-            ref event,
-            window_id,
-        } if window_id == state.window().id() => {
-            if !state.event(event) {
-                match event {
-                    WindowEvent::CloseRequested
-                    | WindowEvent::KeyboardInput {
-                        input:
-                            KeyboardInput {
-                                state: ElementState::Pressed,
-                                virtual_keycode: Some(VirtualKeyCode::Escape),
-                                ..
-                            },
-                        ..
-                    } => *control_flow = ControlFlow::Exit,
+    pub async fn launch(&mut self, config: Config) -> Result<()> {
+        let (width, height) = (config.width, config.height);
 
-                    WindowEvent::Resized(physical_size) => {
-                        let _ = state.resize(*physical_size);
+        let event_loop = EventLoop::new();
+
+        let window = WindowBuilder::new()
+            .with_inner_size(PhysicalSize::new(width, height))
+            .with_title(config.window_name)
+            .build(&event_loop)?;
+
+        let state = Arc::new(Mutex::new(State::new(window).await?));
+
+        self.state = Some(state.clone());
+
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent {
+                ref event,
+                window_id,
+            } if window_id == state.lock().unwrap().window().id() => {
+                if !state.lock().unwrap().event(event) {
+                    match event {
+                        WindowEvent::CloseRequested
+                        | WindowEvent::KeyboardInput {
+                            input:
+                                KeyboardInput {
+                                    state: ElementState::Pressed,
+                                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                                    ..
+                                },
+                            ..
+                        } => *control_flow = ControlFlow::Exit,
+    
+                        WindowEvent::Resized(physical_size) => {
+                            let _ = state.lock().unwrap().resize(*physical_size);
+                        }
+    
+                        WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                            let _ = state.lock().unwrap().resize(**new_inner_size);
+                        }
+    
+                        _ => {}
                     }
-
-                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                        let _ = state.resize(**new_inner_size);
-                    }
-
-                    _ => {}
                 }
             }
-        }
-
-        Event::RedrawRequested(window_id) if window_id == state.window().id() => {
-            state.update();
-
-            match state.render() {
-                Ok(_) => {}
-
-                Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
-                    state.resize(state.size).unwrap();
+    
+            Event::RedrawRequested(window_id) if window_id == state.lock().unwrap().window().id() => {
+                state.lock().unwrap().update();
+    
+                match state.lock().unwrap().render() {
+                    Ok(_) => {}
+    
+                    Err(wgpu::SurfaceError::Lost | wgpu::SurfaceError::Outdated) => {
+                        state.lock().unwrap().resize(state.lock().unwrap().size).unwrap();
+                    }
+    
+                    Err(wgpu::SurfaceError::OutOfMemory) => {
+                        error!("Ran out of memory");
+                        *control_flow = ControlFlow::Exit
+                    }
+    
+                    Err(wgpu::SurfaceError::Timeout) => warn!("Surface timeout"),
                 }
-
-                Err(wgpu::SurfaceError::OutOfMemory) => {
-                    error!("Ran out of memory");
-                    *control_flow = ControlFlow::Exit
-                }
-
-                Err(wgpu::SurfaceError::Timeout) => warn!("Surface timeout"),
             }
-        }
-        Event::RedrawEventsCleared => {
-            state.window().request_redraw();
-        }
-
-        _ => {}
-    });
+            Event::RedrawEventsCleared => {
+                state.lock().unwrap().window().request_redraw();
+            }
+    
+            _ => {}
+        });
+    }
 }
