b433cfb5cbc04b66cc81dde01c3245601b8d0e94
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-11 14:33:49 +0100

    Create a new state

--- src/booster.rs
+++ src/booster.rs
@@ -4,22 +4,32 @@ use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::WindowBuilder;
 
 use crate::config::Config;
+use crate::state::State;
+
+use std::sync::{Arc, Mutex};
 
 use anyhow::Result;
 
-#[derive(Default)]
-pub struct Booster {}
+pub struct Booster {
+    state: Arc<Mutex<State>>,
+}
 
 impl Booster {
-    pub fn launch(config: Config) -> Result<()> {
+    pub fn new(config: Config) -> Result<Booster> {
         let (width, height) = (config.width, config.height);
 
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new()
             .with_inner_size(PhysicalSize::new(width, height))
             .with_title(config.title)
-            .build(&event_loop);
+            .build(&event_loop)?;
 
-        todo!("Launch the state");
+        let state = Arc::new(Mutex::new(pollster::block_on(State::new(window))?));
+
+        Ok(Self { state })
+    }
+
+    pub fn launch(&self) {
+        
     }
 }
