823e2b5d4fd114b0ca016f40bb158bda36606ac0
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-11 13:14:28 +0100

    Create a PipelineComposer to create custom pipelines

---
+++ src/pipeline_composer.rs
@@ -0,0 +1,92 @@
+use wgpu::{Device, FragmentState, RenderPipeline, SurfaceConfiguration, ShaderModuleDescriptor, ShaderSource, PipelineLayoutDescriptor, RenderPipelineDescriptor, VertexState, BlendState, BlendComponent, ColorTargetState, ColorWrites, PrimitiveState, PrimitiveTopology, FrontFace, Face, PolygonMode, MultisampleState};
+use std::{fs, sync::Arc, path::PathBuf};
+use tracing::info;
+
+use anyhow::Result;
+
+pub struct PipelineComposer {
+    pub(crate) pipeline: Option<RenderPipeline>,
+
+    config: SurfaceConfiguration,
+    device: Arc<Device>,
+}
+
+impl PipelineComposer {
+    pub fn new(device: Arc<Device>, config: SurfaceConfiguration) -> PipelineComposer {
+        Self {
+            pipeline: None,
+            config,
+            device,
+        }
+    }
+
+    pub fn new_pipeline(&mut self, path: PathBuf) -> Result<()> {
+        let source = fs::read_to_string(&path)?;
+
+        info!("Reader shader source {path:?}");
+
+        let shader = self.device.create_shader_module(ShaderModuleDescriptor {
+            label: Some("Shader"),
+            source: ShaderSource::Wgsl(source.into())
+        });
+
+        let layout = self.device.create_pipeline_layout(&PipelineLayoutDescriptor {
+            label: Some("Render Pipeline Lay"),
+            bind_group_layouts: &[],
+            push_constant_ranges: &[],
+        });
+
+        let pipeline = self.device.create_render_pipeline(&RenderPipelineDescriptor {
+            label: Some("Render Pipeline"),
+            layout: Some(&layout),
+
+            vertex: VertexState {
+                module: &shader,
+
+                entry_point: "vs_main",
+                buffers: &[],
+            },
+
+            fragment: Some(FragmentState {
+                module: &shader,
+
+                entry_point: "fs_main",
+                targets: &[Some(ColorTargetState {
+                    format: self.config.format,
+                    blend: Some(BlendState {
+                        color: BlendComponent::REPLACE,
+                        alpha: BlendComponent::REPLACE,
+                    }),
+
+                    write_mask: ColorWrites::ALL,
+                })],
+            }),
+
+            primitive: PrimitiveState {
+                topology: PrimitiveTopology::TriangleList,
+                strip_index_format: None,
+
+                front_face: FrontFace::Ccw,
+                cull_mode: Some(Face::Back),
+                polygon_mode: PolygonMode::Fill,
+                unclipped_depth: false,
+                conservative: false,
+            },
+
+            depth_stencil: None,
+
+            multisample: MultisampleState {
+                alpha_to_coverage_enabled: false,
+
+                mask: !0,
+                count: 1,
+            },
+
+            multiview: None,
+        });
+
+        self.pipeline = Some(pipeline);
+
+        Ok(())
+    }
+}
