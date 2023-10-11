54003addd762195a1187e398b101e0064a60f3f1
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-11 11:52:53 +0100

    Add a logging setup function

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,22 @@
+use tracing_subscriber::fmt::time::LocalTime;
+use time::macros::format_description;
+use tracing::Level;
+
 /// The main struct for the engine.
 #[derive(Default)]
 pub struct Llanite();
 
 impl Llanite {
+    pub fn enable_logger() {
+        let timer = LocalTime::new(format_description!("[hour]:[minute]:[second]"));
+
+        tracing_subscriber::fmt()
+            .with_max_level(Level::INFO)
+            .with_thread_names(true)
+            .with_timer(timer)
+            .init();
+    }
+
     pub fn start(&self) {
         todo!("Start the engine")
     }
