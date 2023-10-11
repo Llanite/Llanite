ec29590123693e6cc2b0e311d3a8ef0cb433137b
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-11 13:28:02 +0100

    Reformat pipeline composer

--- src/pipeline_composer.rs
+++ src/pipeline_composer.rs
@@ -1,8 +1,13 @@
-use wgpu::{Device, FragmentState, RenderPipeline, SurfaceConfiguration, ShaderModuleDescriptor, ShaderSource, PipelineLayoutDescriptor, RenderPipelineDescriptor, VertexState, BlendState, BlendComponent, ColorTargetState, ColorWrites, PrimitiveState, PrimitiveTopology, FrontFace, Face, PolygonMode, MultisampleState};
-use std::{fs, sync::Arc, path::PathBuf};
-use tracing::info;
+use wgpu::{
+    BlendComponent, BlendState, ColorTargetState, ColorWrites, Device, Face, FragmentState,
+    FrontFace, MultisampleState, PipelineLayoutDescriptor, PolygonMode, PrimitiveState,
+    PrimitiveTopology, RenderPipeline, RenderPipelineDescriptor, ShaderModuleDescriptor,
+    ShaderSource, SurfaceConfiguration, VertexState,
+};
 
+use std::{fs, path::PathBuf, sync::Arc};
 use anyhow::Result;
+use tracing::info;
 
 pub struct PipelineComposer {
     pub(crate) pipeline: Option<RenderPipeline>,
@@ -27,63 +32,67 @@ impl PipelineComposer {
 
         let shader = self.device.create_shader_module(ShaderModuleDescriptor {
             label: Some("Shader"),
-            source: ShaderSource::Wgsl(source.into())
+            source: ShaderSource::Wgsl(source.into()),
         });
 
-        let layout = self.device.create_pipeline_layout(&PipelineLayoutDescriptor {
-            label: Some("Render Pipeline Lay"),
-            bind_group_layouts: &[],
-            push_constant_ranges: &[],
-        });
+        let layout = self
+            .device
+            .create_pipeline_layout(&PipelineLayoutDescriptor {
+                label: Some("Render Pipeline Lay"),
+                bind_group_layouts: &[],
+                push_constant_ranges: &[],
+            });
 
-        let pipeline = self.device.create_render_pipeline(&RenderPipelineDescriptor {
-            label: Some("Render Pipeline"),
-            layout: Some(&layout),
+        let pipeline = self
+            .device
+            .create_render_pipeline(&RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&layout),
 
-            vertex: VertexState {
-                module: &shader,
+                vertex: VertexState {
+                    module: &shader,
 
-                entry_point: "vs_main",
-                buffers: &[],
-            },
+                    entry_point: "vs_main",
+                    buffers: &[],
+                },
 
-            fragment: Some(FragmentState {
-                module: &shader,
+                fragment: Some(FragmentState {
+                    module: &shader,
 
-                entry_point: "fs_main",
-                targets: &[Some(ColorTargetState {
-                    format: self.config.format,
-                    blend: Some(BlendState {
-                        color: BlendComponent::REPLACE,
-                        alpha: BlendComponent::REPLACE,
-                    }),
+                    entry_point: "fs_main",
+                    targets: &[Some(ColorTargetState {
+                        format: self.config.format,
+                        blend: Some(BlendState {
+                            color: BlendComponent::REPLACE,
+                            alpha: BlendComponent::REPLACE,
+                        }),
 
-                    write_mask: ColorWrites::ALL,
-                })],
-            }),
+                        write_mask: ColorWrites::ALL,
+                    })],
+                }),
 
-            primitive: PrimitiveState {
-                topology: PrimitiveTopology::TriangleList,
-                strip_index_format: None,
+                primitive: PrimitiveState {
+                    topology: PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
 
-                front_face: FrontFace::Ccw,
-                cull_mode: Some(Face::Back),
-                polygon_mode: PolygonMode::Fill,
-                unclipped_depth: false,
-                conservative: false,
-            },
+                    front_face: FrontFace::Ccw,
+                    cull_mode: Some(Face::Back),
+                    polygon_mode: PolygonMode::Fill,
+                    unclipped_depth: false,
+                    conservative: false,
+                },
 
-            depth_stencil: None,
+                depth_stencil: None,
 
-            multisample: MultisampleState {
-                alpha_to_coverage_enabled: false,
+                multisample: MultisampleState {
+                    alpha_to_coverage_enabled: false,
 
-                mask: !0,
-                count: 1,
-            },
+                    mask: !0,
+                    count: 1,
+                },
 
-            multiview: None,
-        });
+                multiview: None,
+            });
 
         self.pipeline = Some(pipeline);
 
