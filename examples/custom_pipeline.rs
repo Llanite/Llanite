56df5c83a3f5b0cc0058a10d962e77a8ab50be44
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-12 15:01:46 +0100

    Use the default logger in the custom pipeline example

--- examples/custom_pipeline.rs
+++ examples/custom_pipeline.rs
@@ -4,9 +4,6 @@ use tracing::Level;
 fn main() {
     let mut llanite = Llanite::default();
 
-    // Llanite::enable_logger(Level::WARN);
-    Llanite::enable_logger(Level::ERROR);
-
     llanite.set_pipeline("./shaders/custom.wgsl".into());
 
     llanite.start(Config::default());
