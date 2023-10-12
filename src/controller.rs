b73f3e62b3719af4c426380c4d86db1daf613f4c
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-12 15:00:59 +0100

    Add a controller to store launch stages

---
+++ src/controller.rs
@@ -0,0 +1,17 @@
+use crate::state::State;
+
+/// Contains all of the startup code.
+/// Each function contained here will be ran before the event loop.
+#[derive(Default)]
+pub struct Controller {
+    pub stages: Vec<Box<dyn Fn(&'static mut State)>>
+}
+
+impl Controller {
+    pub fn add_stage<F>(&mut self, f: F)
+    where
+        F: Fn(&'static mut State) + 'static,
+    {
+        self.stages.push(Box::new(f));
+    }
+}
