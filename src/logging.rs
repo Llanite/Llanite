41fc0f89687fd439214ea1ba5056f6861c481bae
Ben McAvoy <ben.mcavoy@tutanota.com>
2023-10-16 13:05:11 +0100

    Filter different crates

--- src/logging.rs
+++ src/logging.rs
@@ -1,6 +1,7 @@
 use time::macros::format_description;
 use tracing::info;
 use tracing_subscriber::fmt::time::LocalTime;
+use tracing_subscriber::EnvFilter;
 
 use crate::LogConfig;
 
@@ -12,12 +13,19 @@ pub fn enable_logging(log_config: Option<LogConfig>) {
         Some(v) => v,
     };
 
-    tracing_subscriber::fmt()
+    let filter = EnvFilter::from_default_env()
+        .add_directive("wgpu_core=error".parse().unwrap())
+        .add_directive(format!("llanite={}", log_config.level).parse().unwrap());
+
+    let result = tracing_subscriber::fmt()
         .with_thread_names(log_config.thread_names)
         .with_line_number(log_config.line_numbers)
         .with_max_level(log_config.level)
+        .with_env_filter(filter)
         .with_timer(timer)
-        .init();
+        .try_init();
 
-    info!("Logging started with config: {log_config:#?}\n")
+    if result.is_ok() {
+        info!("Logging started with config: {log_config:#?}\n")
+    }
 }
